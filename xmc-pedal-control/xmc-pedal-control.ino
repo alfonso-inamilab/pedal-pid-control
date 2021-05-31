58fcf4ff47b074e7377c1d7355fa7e15374c82f5
java311 <ponchobalandra@gmail.com>
2021-05-31 15:20:53 +0900

    commit to pid controler #1

--- xmc-pedal-control/xmc-pedal-control.ino
+++ xmc-pedal-control/xmc-pedal-control.ino
@@ -1,17 +1,28 @@
 #include <TLE5012-ino.hpp>
-//#include <PID_v1.h>
+#include <PID_v1.h>
 
-#define PWM1_A 11
-#define PWM1_B 12
-#define PWM1_C 13
+// This version implements a very basic PID for the ESCON and Flat Maxxon motor. 
+// The library was taken from here: https://playground.arduino.cc/Code/PIDLibrary/
+// And I follow this example here:  https://playground.arduino.cc/Code/PIDLibraryAdaptiveTuningsExample/
+
+// TODO Tune the parameters using the Ziegler–Nichols method: 
+// https://en.wikipedia.org/wiki/Ziegler%E2%80%93Nichols_method
+// https://robotics.stackexchange.com/questions/167/what-are-good-strategies-for-tuning-pid-loops
+
+#define PWM1_A 11  //pin for the velocity
+#define PWM1_B 12  //pin for the rotation direction
+#define PWM1_C 13  //pin for the ON/OFF switch 
 
 Tle5012Ino Tle5012Sensor = Tle5012Ino();
 errorTypes checkError = NO_ERROR;
 int CALIB_CYCLES = 10; //Number of initial cycles to average the calib value
 int cycles_count =0;  //cycles counter for to calculate the calib value
 
-// This version just displays the magnetic angle sensor from the XMC to the serial terminal 
-// No motor control has been implemented yet.
+// PID controller variables 
+double Setpoint, Input, Output;             //goal,input and output
+double consKp=1, consKi=0.05, consKd=0.25;  //tunning parameters 
+
+PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);  //Specify the links and initial tuning parameters
 
 void setup() {
   delay(2000);
@@ -24,34 +35,28 @@ void setup() {
   pinMode(PWM1_A, OUTPUT);
   pinMode(PWM1_B, OUTPUT);
   pinMode(PWM1_C, OUTPUT);
-  
-  delay(1000);
+
+  Setpoint = -60;  //PID goal angle
+  Tle5012Sensor.getAngleValue(Input);  //Inital angle 
+  myPID.SetMode(AUTOMATIC);  //turn the PID on
+  myPID.SetTunings(consKp, consKi, consKd);   //set the PID tunning parameters
   Serial.println("Init done");
 }
 
-
-double angle = 0.0;
-double avg = 0.0;
-double out = 0.0;
-double OFFSET = 100.0;
+int rotDirection = 255;  
 void loop() {
-  Tle5012Sensor.getAngleValue(angle);  //getting angle from the sensor 
+    Tle5012Sensor.getAngleValue(Input);  //getting angle from the sensor 
   
-  // This block is used to calibrate the initial position of the pedal
-  if (cycles_count < CALIB_CYCLES){
-      avg = avg + angle;
-      cycles_count++;
-  }else if (cycles_count == CALIB_CYCLES){
-      avg = avg / CALIB_CYCLES;
-      cycles_count++;
-  }else if (cycles_count > CALIB_CYCLES){
-    angle = angle - avg;  //applying the calibration value, to get the real angle
+    Serial.print("°C\tangle:");  Serial.print(Output);   // FOR DEBUG ONLY 
+    Serial.println("");                                 // FOR DEBUG ONLY 
 
-    Serial.print("°C\tangle:");  Serial.print(angle);
-    Serial.print("°C\tavg:");  Serial.print(avg);
-    Serial.println("");
-    analogWrite(PWM1_A, angle);  //this doesnt work 
-    analogWrite(PWM1_B, 255);
-    analogWrite(PWM1_C, 0);
-  }
+    myPID.Compute();
+    if (Output > 0){
+      rotDirection = 255; 
+    }else 
+      rotDirection = 0;
+
+    analogWrite(PWM1_A, Output);  //send the PWM to the epos
+    analogWrite(PWM1_B, rotDirection);  // clockwise   
+    analogWrite(PWM1_C, 255);    // Turn on the motor all the time
 }
